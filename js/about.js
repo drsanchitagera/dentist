04e01cd1e64c32bb4953bbf3279c1ba225976e01
unknown <ayush11srs11@gmail.com>
2026-01-26 16:53:17 +0530

    add-about-us

---
+++ js/about.js
@@ -0,0 +1,20 @@
+const aboutHTML = `
+<div class="container-fluid py-6 bg-light">
+            <div class="container">
+                <div class="row g-5 align-items-center">
+                    <div class="col-lg-5 wow bounceInUp" data-wow-delay="0.1s">
+                        <img src="img/dr_sanchita_ger.png" class="img-fluid rounded" alt="" height="400" width="500px">
+                    </div>
+                    <div class="col-lg-7 wow bounceInUp" data-wow-delay="0.3s">
+                        <small class="d-inline-block fw-bold text-dark text-uppercase bg-light border border-primary rounded-pill px-4 py-1 mb-3">About Us</small>
+                        <h1 class="display-5 mb-4">Hi, I’m Dr. Sanchita Gera.</h1>
+                        <p class="mb-4">Dentistry can feel overwhelming at any stage—whether you’re a BDS student, a fresh graduate, or a practicing clinician. I’ve stood exactly where many of you are—confused, questioning, and trying to find clarity beyond textbooks.</p>
+<p class="mb-4">This space was created with a simple intention: to make dentistry feel less intimidating and more understandable. Through handwritten clinical notes, practical explanations, and evidence-based tutorials, I aim to bridge the gap between theory and real-life practice.</p>
+<p class="mb-4">If even one note helps you feel more confident in the clinic or reassures you that you’re on the right path, then this platform has done what it was meant to do. 🦷🤍</p>
+                        <a href="" class="btn btn-primary py-3 px-5 rounded-pill">About Us<i class="fas fa-arrow-right ps-2"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+`
+document.getElementById("about").innerHTML = aboutHTML;    
--- js/header.js
+++ js/header.js
@@ -1,6 +1,6 @@
 
 const headerHTML = `
-  <nav class="navbar navbar-light navbar-expand-lg py-4">
+<nav class="navbar navbar-light navbar-expand-lg py-4">
     <a href="index.html" class="navbar-brand">
         <h1 class="text-primary fw-bold mb-0">Dr Sanchita <span class="text-dark">Gera</span> </h1>
     </a>
@@ -9,29 +9,29 @@ const headerHTML = `
     </button>
     <div class="collapse navbar-collapse" id="navbarCollapse">
         <div class="navbar-nav mx-auto">
-            <a href="index.html" class="nav-item nav-link active">Home</a>
-            <a href="about.html" class="nav-item nav-link">About Us</a>
+            <a href="index.html" id="home" class="nav-item nav-link">Home</a>
+            <a href="about.html" id="about_us" class="nav-item nav-link">About Us</a>
             <div class="nav-item dropdown">
                 <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Notes/eBooks</a>
                 <div class="dropdown-menu bg-light">
-                    <a href="book.html" class="dropdown-item">Free</a>
-                    <a href="blog.html" class="dropdown-item">Paid</a>
+                    <a href="book.html" class="dropdown-item" onclick="setActive(this)">Free</a>
+                    <a href="blog.html" class="dropdown-item" onclick="setActive(this)">Paid</a>
                 </div>
             </div>
             <div class="nav-item dropdown">
                 <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Tutorials</a>
                 <div class="dropdown-menu bg-light">
-                    <a href="book.html" class="dropdown-item">For Dentists</a>
-                    <a href="blog.html" class="dropdown-item">For General Public</a>
+                    <a href="book.html" class="dropdown-item" onclick="setActive(this)">For Dentists</a>
+                    <a href="blog.html" class="dropdown-item" onclick="setActive(this)">For General Public</a>
                 </div>
             </div>        
-            <a href="contact.html" class="nav-item nav-link">Contact Us</a>
+            <a href="contact.html" class="nav-item nav-link" onclick="setActive(this)">Contact Us</a>
         </div>
-        <!-- <button class="btn-search btn btn-primary btn-md-square me-4 rounded-circle d-none d-lg-inline-flex" data-bs-toggle="modal" data-bs-target="#searchModal"><i class="fas fa-search"></i></button> -->
-        <a href="" class="btn btn-primary py-2 px-4 d-none d-xl-inline-block rounded-pill">Book Now</a> 
-    <!-- </div> -->
+        <div class="d-flex justify-content-end">
+            <a href="" class="btn btn-primary py-2 px-4 d-none d-xl-inline-block rounded-pill">Book Now</a>
+        </div>
+    </div>
 </nav>
 `
-
-
 document.getElementById("header").innerHTML = headerHTML;
+
